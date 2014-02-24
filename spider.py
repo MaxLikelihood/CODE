3a77e98656ef6aae54d6520cbfceffb194274515
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-24 00:51:30 -0500

    Add web page information required by spider

--- spider.py
+++ spider.py
@@ -2,3 +2,9 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 
+class DatasetSpider(CrawlSpider):
+
+    name = 'dataset'
+    allowed_domains = ['data.gc.ca']
+    start_urls = ['http://data.gc.ca/data/en/dataset?page=1']
+
