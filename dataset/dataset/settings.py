edab09ea348294f26b63ea575670bd5af7c75502
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-24 02:29:01 -0500

    Create new scrapy project for scraping

---
+++ dataset/dataset/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for dataset project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'dataset'
+
+SPIDER_MODULES = ['dataset.spiders']
+NEWSPIDER_MODULE = 'dataset.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'dataset (+http://www.yourdomain.com)'
