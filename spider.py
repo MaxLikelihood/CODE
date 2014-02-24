28319e889fe911c1cf59cd35b01a18f16c8b4844
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-24 01:40:53 -0500

    Add parsing function definition

--- spider.py
+++ spider.py
@@ -1,6 +1,7 @@
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
+from dataset import DatasetItem
 
 class DatasetSpider(CrawlSpider):
 
@@ -9,3 +10,8 @@ class DatasetSpider(CrawlSpider):
     start_urls = ['http://data.gc.ca/data/en/dataset?page=1']
     rules = [Rule(SgmlLinkExtractor(allow=['/dataset/[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}']),
                   'parse_dataset')]
+
+    def parse_dataset(self, response):
+        sel = Selector(response)
+        dataset = DatasetItem()
+        
