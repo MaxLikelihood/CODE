fb8c2fb065449a436dd8ffa11b469bb2f22a9ad1
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-24 01:36:01 -0500

    Add web crawling rule and dataset url regex exp

--- spider.py
+++ spider.py
@@ -7,4 +7,5 @@ class DatasetSpider(CrawlSpider):
     name = 'dataset'
     allowed_domains = ['data.gc.ca/data/en']
     start_urls = ['http://data.gc.ca/data/en/dataset?page=1']
-
+    rules = [Rule(SgmlLinkExtractor(allow=['/dataset/[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}']),
+                  'parse_dataset')]
