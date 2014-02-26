d6459fdc145cd51e9d479749c0e2f4f85b84cfe4
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-25 20:40:49 -0500

    Add print by 'Continual' frequency filter

--- dataset/print.py
+++ dataset/print.py
@@ -3,3 +3,7 @@ import json
 with open('short_dataset_item.json') as dataset_file:
     dataset = json.load(dataset_file)
 
+for i in range(len(dataset)):
+    if 'Continual' == dataset[i]['frequency']:
+        print dataset[i]['name']
+
