7733a84dc95d43070f476be42a3559b1a2a16ec0
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-28 10:41:56 -0500

    Update to line by line dataset JSON file parsing
    New format under Feed Exports

--- dataset/print.py
+++ dataset/print.py
@@ -1,7 +1,12 @@
 import json
 
-with open('dataset_item.json') as dataset_file:
-    dataset = json.load(dataset_file)
+dataset = []
+dataset_files = ['dataset_item.json']
+
+for f in dataset_files:
+    with open(f) as file:
+        for line in file:
+            dataset.append(json.loads(file))
 
 for i in range(len(dataset)):
     if 'Continual' == dataset[i]['frequency']:
