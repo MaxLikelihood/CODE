94f3b0ba149e367ddf13bf3cd096702067906d61
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-28 10:43:55 -0500

    Fix typo in json.loads
    Referred to file object instead of line within file

--- dataset/print.py
+++ dataset/print.py
@@ -6,7 +6,7 @@ dataset_files = ['dataset_item.json']
 for f in dataset_files:
     with open(f) as file:
         for line in file:
-            dataset.append(json.loads(file))
+            dataset.append(json.loads(line))
 
 for i in range(len(dataset)):
     if 'Continual' == dataset[i]['frequency']:
